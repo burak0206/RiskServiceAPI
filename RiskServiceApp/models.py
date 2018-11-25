c5f974b19d05c29dafba278ab014ede3a5f73807
burak0206 <burak0206@gmail.com>
2018-11-25 19:42:32 +0100

    add log row and block model

--- RiskServiceApp/models.py
+++ RiskServiceApp/models.py
@@ -1,6 +1,15 @@
-from django.db import models
+class LogRow:
+    def __init__(self, date, time, vm_name, vm_id, log_message,):
+        self.date = date
+        self.time = time
+        self.vm_name = vm_name
+        self.vm_id = vm_id
+        self.log_message = log_message
 
-class Person:
-    def __init__(self, username, password):
-        self.u = username
-        self.p = password
+
+class LogBlock:
+    def __init__(self, log_rows):
+        self.log_rows = log_rows
+
+    def add_log_rows(self, log_row):
+        self.log_rows.append(log_row)

932a26e77d99c9601bd9283033a88f159d53b055
burak0206 <burak0206@gmail.com>
2018-11-29 15:44:44 +0100

    RiskServiceApiTests are changed

--- RiskServiceApp/tests.py
+++ RiskServiceApp/tests.py
@@ -5,15 +5,17 @@ from django.test import Client
 
 class RiskServiceApiTests(SimpleTestCase):
 
-    def test_home_page_status_code(self):
+    def test_index_page(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            [{}]
+        )
 
-    def test_view_url_by_name(self):
+    def test_is_user_know_when_username_not_exist(self):
         c = Client()
         response = c.get('/risk/isuserknown')
-        print(response)
-        print(response.content)
         self.assertEqual(response.content.decode("utf-8"), '[{"Error": "username is must"}]')
         self.assertJSONEqual(
             str(response.content, encoding='utf8'),
